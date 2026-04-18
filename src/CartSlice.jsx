7d3c5a01c101f15b9fe90858d798b9e292e9e7a9
Darshan Pawar <darshan.pawar23@gmail.com>
2026-04-18 21:59:49 +0530

    fix CartSlice functions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -8,7 +8,7 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state, action) => {
+    addItem: (state, action) => {
       const item = state.items.find(i => i.id === action.payload.id);
       if (item) {
         item.quantity += 1;
@@ -17,29 +17,18 @@ const cartSlice = createSlice({
       }
     },
 
-    removeFromCart: (state, action) => {
+    removeItem: (state, action) => {
       state.items = state.items.filter(i => i.id !== action.payload);
     },
 
-    increaseQuantity: (state, action) => {
-      const item = state.items.find(i => i.id === action.payload);
-      if (item) item.quantity += 1;
-    },
-
-    decreaseQuantity: (state, action) => {
-      const item = state.items.find(i => i.id === action.payload);
-      if (item && item.quantity > 1) {
-        item.quantity -= 1;
+    updateQuantity: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload.id);
+      if (item) {
+        item.quantity = action.payload.quantity;
       }
     }
   }
 });
 
-export const {
-  addToCart,
-  removeFromCart,
-  increaseQuantity,
-  decreaseQuantity
-} = cartSlice.actions;
-
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
