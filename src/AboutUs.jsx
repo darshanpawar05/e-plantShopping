d0a60c34a853bd1fe425274a4ae8acc6e0e49ac4
Darshan Pawar <darshan.pawar23@gmail.com>
2026-04-18 21:20:26 +0530

    added AboutUs file

---
+++ src/AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>We are a plant shop providing beautiful indoor and outdoor plants.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
